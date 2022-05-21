1c8a48ecd9a26f4a271539860271973d5b1c6f3c
inmacc96 <inmacaballerocarrero@gmail.com>
2022-05-21 16:35:09 +0200

    Add the FileUploader component for drag and drop of files.

--- frontend/src/pages/FileUpload.jsx
+++ frontend/src/pages/FileUpload.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+
 const FileUpload = () => {
-  return <div>FileUpload</div>;
+  const [file, setFile] = useState([]);
+
+  const handleChange = (file) => {
+    setFile(file);
+  };
+
+  return (
+    <div>
+      <h1>Hello To Drag & Drop Files</h1>
+      <FileUploader
+        name="file"
+        multiple={true}
+        hoverTitle="Drop here"
+        handleChange={handleChange}
+      />
+    </div>
+  );
 };
 
 export default FileUpload;
