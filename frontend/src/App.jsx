36e71a3b9902ffd70f052653ac16fbe73be0101f
inmacc96 <inmacaballerocarrero@gmail.com>
2022-05-21 16:26:18 +0200

    Delete App.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Login from "./pages/Login/Login";
 import FileUpload from "./pages/FileUpload";
-import "./App.css";
 
 function App() {
   return (
