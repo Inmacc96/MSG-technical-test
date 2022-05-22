349a861684ec3a21eaa959961afc3b7f9e5289b4
inmacc96 <inmacaballerocarrero@gmail.com>
2022-05-22 22:29:03 +0200

    Refactor code - Change the app's routing system

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./layouts/Layout";
 import Login from "./pages/Login/Login";
 import FileUpload from "./pages/FileUpload/FileUpload";
 
@@ -7,10 +6,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Login />} />
-          <Route path="/fileUpload" element={<FileUpload />} />
-        </Route>
+        <Route path="/" element={<Login />} />
+        <Route path="/fileUpload" element={<FileUpload />} />
       </Routes>
     </BrowserRouter>
   );
